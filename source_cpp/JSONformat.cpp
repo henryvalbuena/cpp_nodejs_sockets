a223a13cb0c0540d493555370378545d4a9bf57b
Henry Valbuena <dev.henryvm@gmail.com>
2019-02-16 14:19:22 -0500

    Updated JSON Ctor

--- source_cpp/JSONformat.cpp
+++ source_cpp/JSONformat.cpp
@@ -1,76 +1,152 @@
 #include "JSONformat.h"
 
 namespace sict {
-    JSONformat::~JSONformat() {
-        delete [] property;
-        delete [] value;
-        delete [] JSON;
-        property = nullptr;
-        value = nullptr;
-        JSON = nullptr;
-    }
-    int JSONformat::addPropertyAndValue(const char* prop, const char* val) {
-        if (strVal(prop) && size == 0) {
-            property = new std::string[std::strlen(prop)];
-            property[size] = prop;
-            value = new std::string[std::strlen(val)];
-            value[size] = val;
-            size++;
-            return 1;
-        } else if (strVal(prop) && size > 0) {
-            property[size] = prop;
-            value[size] = val;
-            size++;
-            return 1;
-        }
-        return -1;
-    }
-    int JSONformat::updateProperty(const char* prop, size_t pos) {
-        if (strVal(prop) && pos < size) {
-            property[pos] = prop;
-            return 1;
-        }
-        return -1;
-    }
-    int JSONformat::updateValue(const char* val, size_t pos) {
-        if (strVal(val) && pos < size) {
-            value[pos] = val;
-            return 1;
-        }
-        return -1;
-    }
-    int JSONformat::removePropertyAndValue(size_t pos) {
-        std::cout << "TO BE IMPLEMENTED\n";
-        return 0;
-    }
-    void JSONformat::display() const {
-        std::cout << "'{\n";
-         if (size > 0) {
-            for (int i = 0; i < size; i++) {
-                std::cout << "\"" << property[i] << "\"" << ": " << "\"" << value[i] << "\"";
-                if (i >= 0 && (i + 1) < size) std::cout << ",\n";
-                else std::cout << "\n";
-            }
+	JSONformat::JSONformat(const std::string str) {
+		JSON = nullptr;
+		max = size = 0;
+		isEmpty = true;
+		property = nullptr;
+		value = nullptr;
+		//check if its an array
+		// ******** TO BE IMPLEMENTED ********
+
+		// check if the string has the format {}
+
+		if (str.size() > 0 && str.at(0) == '{' && str.at((str.size() - 1)) == '}') {
+			size_t cnt = 1, k = 0;
+			do
+			{
+				k = str.find(',', (k + 1u));
+				if (k != std::string::npos) {
+					cnt++;
+				}
+			} while (k != std::string::npos);
+			k = 0;
+			property = new std::string[cnt];
+			value = new std::string[cnt];
+			max = cnt;
+			isEmpty = false;
+			if (cnt > 1) {
+				std::string temp = str;
+				temp = temp.erase(0, 1);
+				temp = temp.erase(temp.size() - 1, 1);
+			
+				for (size_t i = 0; i < cnt; i++) {
+					std::string s = temp.substr(0, temp.find(','));
+					size_t pos = s.find(':');
+					addPropertyAndValue(
+						s.substr(1, pos - 2).c_str(),
+						s.substr(pos + 2, s.size() - (pos + 3)).c_str()
+					);
+					temp.erase(0, temp.find(',') + 1);
+				}
+			}
+			else {
+				size_t pos = str.find(':');
+					addPropertyAndValue(
+						str.substr(2, pos - 3).c_str(),
+						str.substr(pos + 2, str.size() - (pos + 4)).c_str()
+					);
+			}
 		}
-        std::cout << "}'\n";
-    }
-    char*& JSONformat::getJSONString() {
-        // '{"property":"value"}'
-        // add the value and the format to the string
-        std::string tempStr = "'{";
-        for (int i = 0; i < size; i++) {
-            tempStr += "\"" + property[i] + "\":\"" + value[i] + "\"";
-            if (i >= 0 && (i + 1) < size) tempStr += ",";
-        }
-        tempStr += "}'";
-        // return a c string
-        JSON = new char[tempStr.size()];
-        std::strcpy(JSON, tempStr.c_str());
-        return JSON;
-    }
-    bool JSONformat::strVal(const char* str) {
-        if (str == nullptr || str[0] == '\0')
-            return false;
-        return true;
-    }
+	}
+	JSONformat::~JSONformat() {
+		delete[] property;
+		delete[] value;
+		delete[] JSON;
+		property = nullptr;
+		value = nullptr;
+		JSON = nullptr;
+	}
+	int JSONformat::addPropertyAndValue(const char* prop, const char* val) {
+		if (strVal(prop) && strVal(val)) { 
+			if (!isEmpty && size < max) {
+				property[size] = prop;
+				value[size] = val;
+				size++;
+				return 1;
+			}
+			else if(!isEmpty && size >= max) {
+				max++;
+				std::string* tempProp = new std::string[max];
+				std::string* tempVal = new std::string[max];
+				for (size_t i = 0; i < size; i++) {
+					tempProp[i] = property[i];
+					tempVal[i] = value[i];
+				}
+				tempProp[size] = prop;
+				tempVal[size] = val;
+				size++;
+				delete [] property;
+				delete [] value;
+				property = tempProp;
+				value = tempVal;
+				return 1;
+			}
+			else {
+				max++;
+				property = new std::string[max];
+				value = new std::string[max];
+				property[size] = prop;
+				value[size] = val;
+				size++;
+				isEmpty = false;
+				return 1;
+			}
+		}
+		return -1;
+	}
+	int JSONformat::updateProperty(const char* prop, size_t pos) {
+		if (strVal(prop) && pos < size) {
+			property[pos] = prop;
+			return 1;
+		}
+		return -1;
+	}
+	int JSONformat::updateValue(const char* val, size_t pos) {
+		if (strVal(val) && pos < size) {
+			value[pos] = val;
+			return 1;
+		}
+		return -1;
+	}
+	int JSONformat::removePropertyAndValue(size_t pos) {
+		std::cout << "TO BE IMPLEMENTED\n";
+		return 0;
+	}
+	void JSONformat::display() const {
+		if (!isEmpty) {
+			std::cout << "{\n";
+			if (size > 0) {
+				for (size_t i = 0; i < size; i++) {
+					std::cout << "\"" << property[i] << "\"" << ": " << "\"" << value[i] << "\"";
+					if (i >= 0 && (i + 1) < size) std::cout << ",\n";
+					else std::cout << "\n";
+				}
+			}
+			std::cout << "}\n";
+		}
+		else std::cout << "Undefined\n";
+	}
+	const char* JSONformat::getJSONString() {
+		if (!isEmpty) {
+			std::string tempStr = "{";
+			for (size_t i = 0; i < size; i++) {
+				tempStr += "\"" + property[i] + "\":\"" + value[i] + "\"";
+				if (i >= 0 && (i + 1) < size) tempStr += ",";
+			}
+			tempStr += "}";
+			JSON = new char[tempStr.size()];
+			std::strcpy(JSON, tempStr.c_str());
+		}
+		return JSON;
+	}
+	bool JSONformat::strVal(const char* str) {
+		if (str == nullptr || str[0] == '\0')
+			return false;
+		return true;
+	}
+	bool JSONformat::empty() const {
+		return isEmpty;
+	}
 }
--- source_cpp/JSONformat.h
+++ source_cpp/JSONformat.h
@@ -1,7 +1,7 @@
 // this class transform c strings into c strings JSON format
 // Example:
 // Input message: "Hello World!"
-// Output message: '{"message": "Hello World!"}'
+// Output message: {"message": "Hello World!"}
 // Additionally, the class has methods to add properties to the JSON object
 // and values to those methods before outputting the final message.
 
@@ -10,33 +10,57 @@
 #include <string>
 #include <cstring>
 #include <iostream>
+
+// Error codes
+#define OK 1
+#define JSON_FORMAT -1
+#define OUT_OB -2
 namespace sict {
     class JSONformat {
         // Property of the JSON obj
         std::string* property;
         // Value of the JSON obj
         std::string* value;
-        // size of the array
+        // Current size of the array
         size_t size;
+        // Maximum size of the array
+        size_t max;
         // JSON return cstring
         char* JSON;
+        // Status of current object
+        bool isEmpty;
         // Validate a c string passed if empty or null
         bool strVal(const char* cstring_);
+        // Holds the error code
+        int err;
     public:
-        JSONformat() : property(nullptr), value(nullptr), size(0), JSON(nullptr) {};
+        JSONformat() : property(nullptr), value(nullptr), size(0), 
+            max(0), JSON(nullptr), isEmpty(true) {};
+        // Construct a JSON object passing a string as argument that
+        // have the format {"property":"value"[,"property":"value"]}
+        JSONformat(const std::string JSON_String);
         ~JSONformat();
-    // Add property and value, return 1 if ok else fails
+        // Add property and value, return 1 if ok else fails
         int addPropertyAndValue(const char* property_, const char* value_);
-    // Modify property, suppling property position in the array, return 1 if ok else fails
+        // Modify property, suppling property position in the array, return 1 if ok else fails
         int updateProperty(const char* property_, size_t position_);
-    // Modify value, supplying property position in the array, return 1 if ok else fails
+        // Modify value, supplying property position in the array, return 1 if ok else fails
         int updateValue(const char* value_, size_t position_);
-    // Remove property and value, supplying position in the array, return 1 if ok else fails
+        // Remove property and value, supplying position in the array, return 1 if ok else fails
         int removePropertyAndValue(size_t position_);
-    // Display current JSON object
+        // Returns std::string property and value at given position
+        // formatted as {"property":"value"}
+        const std::string& getPropertyAndValue(size_t position_) const;
+        // Return std::string value at given position
+        const std::string& getValue(size_t position_) const;
+        // Display current JSON object
         void display() const;
-    // Returns cstring reference of JSON format
-        char*& getJSONString();
+        // Returns cstring reference of JSON format
+        const char* getJSONString();
+        // Returns true if current object is empty
+        bool empty() const;
+        // Returns the current object error state
+        int errorState() const;
     };
 }
 #endif // SICT_JSONformat
